59669bd2947e47c3ff565058a45b6aed8197b66c
Stefan Hudelmaier <stefan.hudelmaier@device-insight.com>
2021-03-10 17:37:47 +0100

    Log request

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,13 +1,22 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    context.log("Context: ", JSON.stringify(context));
+    context.log("Request: ", JSON.stringify(req));
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: JSON.stringify(
+            {
+                value: [{
+                    name: "Test",
+                    properties: {
+                        age: 39
+                    }
+                }, {
+                    name: "Test2",
+                    properties: {
+                        age: 41
+                    }
+                }]
+            })
     };
 }
