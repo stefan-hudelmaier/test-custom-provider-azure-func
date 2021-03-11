056ac3321b18e45c2ce5bd57582ef068c5eef450
Stefan Hudelmaier <stefan.hudelmaier@device-insight.com>
2021-03-11 12:18:01 +0100

    Add initial custom provider url handling

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -3,23 +3,56 @@ module.exports = async function (context, req) {
     context.log("Context: ", JSON.stringify(context, null, 2));
     context.log("Request: ", JSON.stringify(req, null, 2));
 
+    const customProviderRequestPath = req.headers['x-ms-customproviders-requestpath'];
+
+    if (!customProviderRequestPath) {
+        context.res = {
+            status: 400,
+            body: "Header x-ms-customproviders-requestpath is missing"
+        }
+        context.done;
+        return;
+    }
+
+    if (req.method !== 'GET') {
+        context.res = {
+            status: 400,
+            body: "Only GETs are supported currently"
+        }
+        context.done;
+        return;
+    }
+    const isResourceRequest = customProviderRequestPath.split('/').length % 2 === 1;
+
+    let responseBody;
+    if (isResourceRequest) {
+        responseBody = {
+            name: 'Test',
+            properties: {
+                age: 39
+            }
+        }
+    } else {
+        responseBody = {
+            value: [{
+                name: "Test",
+                properties: {
+                    age: 39
+                }
+            }, {
+                name: "Test2",
+                properties: {
+                    age: 41
+                }
+            }]
+        }
+    }
+
     context.res = {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: {
-                value: [{
-                    name: "Test",
-                    properties: {
-                        age: 39
-                    }
-                }, {
-                    name: "Test2",
-                    properties: {
-                        age: 41
-                    }
-                }]
-            }
+        body:  responseBody
     };
 
     context.done();
---
+++ local-function-list-custom-resources.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+http GET :7071/api/HttpTrigger1 x-ms-customproviders-requestpath:/subscriptions/123/resourceGroups/myrg/providers/Microsoft.CustomProviders/resourceProviders/public/users
